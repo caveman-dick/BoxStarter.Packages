100821daadbf05fd43402015553700e9346dca50
Richard Mayes <caveman.dick@gmail.com>
2021-03-29 11:14:05 +0100

    Remove TFPT2015 package from build

--- BuildPackages.ps1
+++ BuildPackages.ps1
@@ -1,5 +1,4 @@
 ﻿Get-ChildItem $Boxstarter.LocalRepo | Remove-Item -Force -Recurse
 New-BoxstarterPackage -Name DevMachineSetup -Path "$(Get-Location)\DevMachineSetup\" -quiet
-New-BoxstarterPackage -Name TFPT2015 -Path "$(Get-Location)\TFPT2015\"
 
 Invoke-BoxStarterBuild -All
