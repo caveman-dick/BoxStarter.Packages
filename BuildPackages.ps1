a37fc14aa3b78440aeeb74eef462b862f32e7064
Richard Mayes <caveman.dick@gmail.com>
2021-07-08 19:01:10 +0100

    fix: remove TFPT2015 package from the build script

--- BuildPackages.ps1
+++ BuildPackages.ps1
@@ -1,6 +1,5 @@
 ﻿Get-ChildItem $Boxstarter.LocalRepo | Remove-Item -Force -Recurse
 New-BoxstarterPackage -Name DevMachineSetup -Path "$(Get-Location)\DevMachineSetup\" -quiet
 New-BoxstarterPackage -Name MSSQL2014-Dev -Path "$(Get-Location)\MSSQL2014-Dev\"
-New-BoxstarterPackage -Name TFPT2015 -Path "$(Get-Location)\TFPT2015\"
 
 Invoke-BoxStarterBuild -All
