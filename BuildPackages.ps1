b69bef71ba1908ff24c5beb9948c731272fd9db1
Richard Mayes <caveman.dick@gmail.com>
2021-07-08 19:00:26 +0100

    chore: remove obsolete VS2015 package

--- BuildPackages.ps1
+++ BuildPackages.ps1
@@ -2,6 +2,5 @@
 New-BoxstarterPackage -Name DevMachineSetup -Path "$(Get-Location)\DevMachineSetup\" -quiet
 New-BoxstarterPackage -Name MSSQL2014-Dev -Path "$(Get-Location)\MSSQL2014-Dev\"
 New-BoxstarterPackage -Name TFPT2015 -Path "$(Get-Location)\TFPT2015\"
-New-BoxstarterPackage -Name VisualStudio2015-Enterprise -Path "$(Get-Location)\VisualStudio2015-Enterprise\"
 
 Invoke-BoxStarterBuild -All
