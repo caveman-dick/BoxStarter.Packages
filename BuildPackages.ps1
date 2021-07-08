1c76440d6956904dc578a72bbe4deffda6f73312
Richard Mayes <caveman.dick@gmail.com>
2021-07-08 19:01:45 +0100

    chore: remove obsolete MSSQL2014

--- BuildPackages.ps1
+++ BuildPackages.ps1
@@ -1,5 +1,4 @@
 ﻿Get-ChildItem $Boxstarter.LocalRepo | Remove-Item -Force -Recurse
 New-BoxstarterPackage -Name DevMachineSetup -Path "$(Get-Location)\DevMachineSetup\" -quiet
-New-BoxstarterPackage -Name MSSQL2014-Dev -Path "$(Get-Location)\MSSQL2014-Dev\"
 
 Invoke-BoxStarterBuild -All
