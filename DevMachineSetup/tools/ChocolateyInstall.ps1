080a3edb80439641b7d04aab2a2c8466464d2368
Richard Mayes <caveman.dick@gmail.com>
2021-07-12 16:40:17 +0100

    style: fix whitespace

--- DevMachineSetup/tools/ChocolateyInstall.ps1
+++ DevMachineSetup/tools/ChocolateyInstall.ps1
@@ -1,14 +1,14 @@
-try 
+try
 {
-    # Boxstarter options 
-    $Boxstarter.RebootOk = $true # Allow reboots? 
-    $Boxstarter.NoPassword = $false # Is this a machine with no login password? 
+    # Boxstarter options
+    $Boxstarter.RebootOk = $true # Allow reboots?
+    $Boxstarter.NoPassword = $false # Is this a machine with no login password?
     $Boxstarter.AutoLogin = $true # Save my password securely and auto-login after a reboot
 
     choco feature enable -n allowGlobalConfirmation
 
     # Basic setup
-    Update-ExecutionPolicy Unrestricted    
+    Update-ExecutionPolicy Unrestricted
     Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar -EnableOpenFileExplorerToQuickAccess -EnableShowRecentFilesInQuickAccess -EnableShowFrequentFoldersInQuickAccess -EnableExpandToOpenFolder -EnableShowRibbon
     Set-BoxstarterTaskbarOptions -Size Large -Dock Bottom -AlwaysShowIconsOn
     Disable-InternetExplorerESC
@@ -24,11 +24,11 @@ try
     if (Test-PendingReboot) { Invoke-Reboot }
 
     Install-WindowsUpdate -AcceptEula
-    
-    if (Test-PendingReboot) { Invoke-Reboot }       
+
+    if (Test-PendingReboot) { Invoke-Reboot }
 
     # Essential Tools
-    choco install 7zip    
+    choco install 7zip
     choco install windirstat
     choco install baretail
     choco install greenshot
@@ -53,7 +53,7 @@ try
     choco install firefox
     choco install google-chrome-x64
     choco install opera
-    
+
     # DevTools - Frameworks
     choco install dotnet #.
     choco install dotnet4.6
@@ -62,26 +62,26 @@ try
     choco install nodejs
 
     if (Test-PendingReboot) { Invoke-Reboot }
-       
+
     #DevTools - Source Control
     choco install git
     choco install git-credential-manager-for-windows
     choco install sourcetree
     choco install poshgit
     choco install p4merge
-        
-    # DevTools - IDE's    
+
+    # DevTools - IDE's
     choco install jetbrainstoolbox
     choco install linqpad6
 
     if (Test-PendingReboot) { Invoke-Reboot }
-    
+
     # DevTools - Misc
     choco install nugetpackageexplorer
     choco install fiddler
     choco install awscli
-    
-    # Virtualisation              
+
+    # Virtualisation
     Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName Containers
     Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName Microsoft-Hyper-V-All
     Enable-WindowsOptionalFeature -Online -NoRestart -FeatureName VirtualMachinePlatform
@@ -96,12 +96,12 @@ try
     Enable-WindowsOptionalFeature -Online -All -NoRestart -FeatureName IIS-ASPNET45
 
     choco install urlrewrite
-    
-    mkdir C:\source        
+
+    mkdir C:\source
 
     Write-ChocolateySuccess 'Dev-Machine-Setup'
-} 
-catch 
+}
+catch
 {
     throw $_.Exception
 }
